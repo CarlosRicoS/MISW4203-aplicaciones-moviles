841eb8769a9d0d588b0b73b5f25605cfd682ef7d
Angel Henao <ay.henao@uniandes.edu.co>
2025-04-16 01:15:50 -0500

    Added ListAlbumsViewModel

---
+++ app/src/main/java/co/edu/uniandes/miso/vinilos/viewmodel/album/ListAlbumsViewModel.kt
@@ -0,0 +1,27 @@
+package co.edu.uniandes.miso.vinilos.viewmodel.album
+
+import androidx.lifecycle.ViewModel
+import co.edu.uniandes.miso.vinilos.model.data.rest.dto.Album.AlbumDTO
+import co.edu.uniandes.miso.vinilos.model.data.rest.serviceadapter.VinylsApiService
+import co.edu.uniandes.miso.vinilos.model.data.rest.serviceadapter.VinylsServiceAdapter
+import co.edu.uniandes.miso.vinilos.model.repository.NetworkVinylsAlbumsRepository
+import co.edu.uniandes.miso.vinilos.model.repository.VinylsAlbumsRepository
+
+class ListAlbumsViewModel : ViewModel() {
+
+    private val apiService: VinylsApiService = VinylsServiceAdapter.instance.vinylsService
+
+    private val albumsRepository: VinylsAlbumsRepository = NetworkVinylsAlbumsRepository(apiService)
+
+    private var albums: MutableList<AlbumDTO> = mutableListOf()
+
+    suspend fun loadAlbums() {
+        albums.clear()
+        albums.addAll(albumsRepository.getVinylsAlbums())
+    }
+
+    fun getAlbums(): List<AlbumDTO> {
+        return albums
+    }
+
+}
