5bfee1888d12b4ab3b0f51785c56a279c15668aa
Angel Henao <ay.henao@uniandes.edu.co>
2025-04-25 22:41:03 -0500

    Add path annotation to getAlbumById method in VinylsApiService

--- app/src/main/java/co/edu/uniandes/miso/vinilos/model/data/rest/serviceadapter/VinylsApiService.kt
+++ app/src/main/java/co/edu/uniandes/miso/vinilos/model/data/rest/serviceadapter/VinylsApiService.kt
@@ -2,11 +2,12 @@ package co.edu.uniandes.miso.vinilos.model.data.rest.serviceadapter
 
 import co.edu.uniandes.miso.vinilos.model.data.rest.dto.album.AlbumDTO
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface VinylsApiService {
     @GET("albums")
     suspend fun getAlbums(): List<AlbumDTO>
 
     @GET("albums/{id}")
-    suspend fun getAlbumById(id: Int): AlbumDTO
+    suspend fun getAlbumById(@Path("id") id: Int): AlbumDTO
 }
--- app/src/main/java/co/edu/uniandes/miso/vinilos/model/data/rest/serviceadapter/VinylsServiceAdapter.kt
+++ app/src/main/java/co/edu/uniandes/miso/vinilos/model/data/rest/serviceadapter/VinylsServiceAdapter.kt
@@ -1,42 +1,17 @@
 package co.edu.uniandes.miso.vinilos.model.data.rest.serviceadapter
 
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 class VinylsServiceAdapter {
-//    TODO: Change this URL when backend is deployed in the cloud
+    //    TODO: Change this URL when backend is deployed in the cloud
     private val baseUrl = "https://backvynils-q6yc.onrender.com/"
-
-    // Create a trust manager that does not validate certificate chains
-    private val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-    })
-
-    // Install the all-trusting trust manager
-    private val sslContext = SSLContext.getInstance("SSL").apply {
-        init(null, trustAllCerts, SecureRandom())
-    }
-
-    // Create an ssl socket factory with our all-trusting manager
-    private val okHttpClient = OkHttpClient.Builder()
-        .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
-        .hostnameVerifier { _, _ -> true }
-        .build()
 
     private val retrofitClient: Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .addConverterFactory(ScalarsConverterFactory.create())
         .baseUrl(baseUrl)
-        .client(okHttpClient)
         .build()
 
     internal val vinylsService: VinylsApiService by lazy {
