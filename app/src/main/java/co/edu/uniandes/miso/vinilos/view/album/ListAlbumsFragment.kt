5f86f3737fcb427dec764a409347778dbdd12ed7
Hector Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2025-05-01 12:05:57 -0500

    HU03 - UI para lista de Artistas (#71)

--- app/src/main/java/co/edu/uniandes/miso/vinilos/view/album/ListAlbumsFragment.kt
+++ app/src/main/java/co/edu/uniandes/miso/vinilos/view/album/ListAlbumsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -51,7 +50,7 @@ class ListAlbumsFragment : Fragment() {
         }
 
         viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
-            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+            binding.albumListProgressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
         }
 
         viewModel.errorMessage.observe(viewLifecycleOwner) { errorMsg ->
---
+++ app/src/main/java/co/edu/uniandes/miso/vinilos/view/performer/ListPerformersFragment.kt
@@ -0,0 +1,69 @@
+package co.edu.uniandes.miso.vinilos.view.performer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.edu.uniandes.miso.vinilos.R
+import co.edu.uniandes.miso.vinilos.databinding.FragmentListPerformersBinding
+import co.edu.uniandes.miso.vinilos.view.adapters.ListPerformersAdapter
+import co.edu.uniandes.miso.vinilos.viewmodel.performer.ListPerformersViewModel
+
+class ListPerformersFragment : Fragment() {
+    private var _binding: FragmentListPerformersBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private val viewModel: ListPerformersViewModel by viewModels()
+    private var viewModelAdapter: ListPerformersAdapter? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListPerformersBinding.inflate(inflater, container, false)
+        viewModelAdapter = ListPerformersAdapter(){ performerId, performerName, performerImage ->
+
+            val bundle = Bundle().apply {
+                putInt("performerId", performerId)
+                putString("performerName", performerName)
+                putString("performerImage", performerImage)
+            }
+            //findNavController().navigate(R.id.albumDetailFragment, bundle)
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.performerRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+
+        viewModel.performers.observe(viewLifecycleOwner) { performers ->
+            viewModelAdapter?.performer = performers
+        }
+
+        viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            binding.performerListProgressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+        }
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) { errorMsg ->
+            errorMsg?.let {
+                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        viewModel.loadPerformers()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
