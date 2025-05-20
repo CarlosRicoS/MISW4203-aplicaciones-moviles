e568a811737605430e88b6dba9856171d0fd09a6
Hernan Dario Jimenez <hd.jimenezl1@uniandes.edu.co>
2025-05-20 09:01:55 -0500

    E2E implementation - Add Coments

--- app/src/androidTest/java/co/edu/uniandes/miso/vinilos/DetailedAlbumTest.kt
+++ app/src/androidTest/java/co/edu/uniandes/miso/vinilos/DetailedAlbumTest.kt
@@ -5,10 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.RootMatchers.isPlatformPopup
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withChild
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -21,6 +31,7 @@ import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Date
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
@@ -218,6 +229,119 @@ class DetailedAlbumTest {
         textView.check(matches(withText("The most relevant album of Ruben Blades")))
     }
 
+    @Test
+    fun addCommentTest() {
+
+        // Given
+        val newComment = "New Comment at -> " + Date().time
+        navigateToCollectorUserFlow()
+        navigateToAlbumsTab()
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.albumsRv),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    1
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(2, click()))
+        Thread.sleep(timeToWait)
+
+        val tabView = onView(
+            allOf(
+                withContentDescription("COMENTARIOS"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.albumDetailTabs),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+
+        tabView.perform(click())
+        Thread.sleep(timeToWait)
+
+        onView(withId(R.id.commentRating)).perform(click())
+        Thread.sleep(timeToWait / 2)
+        onView(withText("4"))
+            .inRoot(isPlatformPopup())
+            .perform(click())
+        Thread.sleep(timeToWait / 2)
+
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.contentInput),
+                childAtPosition(
+                    childAtPosition(
+                        withClassName(`is`("com.google.android.material.textfield.TextInputLayout")),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(replaceText(newComment), closeSoftKeyboard())
+
+        Thread.sleep(timeToWait)
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.addCommentButton), withText("AGREGAR"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.newCommentForm),
+                        childAtPosition(
+                            withClassName(`is`("android.widget.LinearLayout")),
+                            1
+                        )
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+
+        // When
+        materialButton2.perform(click())
+        Thread.sleep(timeToWait)
+
+        // Then
+        val commentView = onView(
+            allOf(
+                IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java),
+                withChild(
+                    allOf(
+                        IsInstanceOf.instanceOf(android.widget.TextView::class.java),
+                        withId(R.id.collector_name), withText("Coleccionista")
+                    )
+                ),
+                withChild(
+                    allOf(
+                        withChild(
+                            allOf(
+                                IsInstanceOf.instanceOf(android.widget.TextView::class.java),
+                                withId(R.id.comment_rating), withText("4")
+                            )
+                        ),
+                        withChild(
+                            allOf(
+                                IsInstanceOf.instanceOf(android.widget.TextView::class.java),
+                                withId(R.id.comment_description), withText(newComment)
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        commentView.check(matches(isDisplayed()))
+    }
+
     private fun childAtPosition(
         parentMatcher: Matcher<View>, position: Int
     ): Matcher<View> {
@@ -234,5 +358,28 @@ class DetailedAlbumTest {
                         && view == parent.getChildAt(position)
             }
         }
+    }
+
+    private fun navigateToCollectorUserFlow() {
+        Thread.sleep(timeToWait)
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        Thread.sleep(1000)
+
+        onView(allOf(withText("Usuario"), withId(R.id.title))).perform(click())
+        Thread.sleep(timeToWait)
+
+        onView(withText("Coleccionista")).perform(click())
+        Thread.sleep(timeToWait)
+
+        onView(withText("Jaime Monsalve")).perform(click())
+        Thread.sleep(timeToWait)
+    }
+
+    private fun navigateToAlbumsTab() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open())
+        Thread.sleep(1000)
+
+        onView(allOf(withText("Albumes"), withId(R.id.title))).perform(click())
+        Thread.sleep(timeToWait * 2)
     }
 }
